209bdd5f1a5811c7619fd8da60c307eabb3a4584
Daniel Huisman <daniel@huisman.me>
2017-02-13 14:15:31 +0100

    Added simple schema builder

--- sepa/group_header.py
+++ sepa/group_header.py
@@ -9,25 +9,15 @@ class GroupHeader:
         self.instructing_party = instructing_party
         self.instructed_party = instructed_party
 
-    def build(self):
-        header = etree.Element('GrpHdr')
-        etree.SubElement(header, 'MsgId').text = self.message_id
-        etree.SubElement(header, 'CreDtTm').text = self.creation_datetime
-
-        if self.authorisation is not None:
-            if isinstance(self.authorisation, []):
-                for element in self.authorisation:
-                    etree.SubElement(header, 'Authstn').text = element
-            else:
-                etree.SubElement(header, 'Authstn').text = self.authorisation
-
-        if self.initiating_party is not None:
-            etree.SubElement(header, 'InitgPty').text = self.initiating_party
-
-        if self.instructing_party is not None:
-            etree.SubElement(header, 'InstgAgt').text = self.instructing_party
-
-        if self.instructed_party is not None:
-            etree.SubElement(header, 'InstdAgt').text = self.instructed_party
-
-        return header
+    def schema(self):
+        return {
+            'tag': 'GrpHdr',
+            'children': {
+                'message_id': 'MsgId',
+                'creation_datetime': 'CreDtTm',
+                'authorisation': ['Authstn'],
+                'initiating_party': 'InitgPty',
+                'instructing_party': 'InstgAgt',
+                'instructed_party': 'InstdAgt'
+            }
+        }
--- sepa/test.py
+++ sepa/test.py
@@ -1,8 +1,33 @@
 from lxml import etree
 from group_header import GroupHeader
 
-header = GroupHeader(message_id='123456', creation_datetime='now', instructing_party='MyVCK')
+def builder(instance):
+    schema = instance.schema()
 
-# doc = etree.ElementTree(header.build())
+    # Create root element
+    element = etree.Element(schema['tag'])
 
-print(etree.tostring(header.build()))
+    # Loop over all schema children
+    for key, value in schema['children'].iteritems():
+        data = getattr(instance, key)
+        if data is not None:
+            # Check if the data can be an array according to the schema
+            if isinstance(value, list):
+                # Check if the data is actually an array
+                if isinstance(data, list):
+                    for d in data:
+                        etree.SubElement(element, value[0]).text = d
+                else:
+                    etree.SubElement(element, value[0]).text = data
+            # Else it's a plain element
+            else:
+                etree.SubElement(element, value).text = data
+
+    return element
+
+def xml_to_string(xml):
+    return etree.tostring(xml)
+
+header = GroupHeader(message_id='123456', creation_datetime='now', instructing_party='MyVCK', authorisation=['test1', 'test2'])
+
+print(xml_to_string(builder(header)))
