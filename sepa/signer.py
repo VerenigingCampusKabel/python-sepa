f96755383c05c9ecf6ee73d38d46fc2e9a0bc87e
Daniel Huisman <daniel@huisman.me>
2017-03-22 21:58:16 +0100

    Pass along keyword arguments from sign/verify calls

--- sepa/signer.py
+++ sepa/signer.py
@@ -3,8 +3,8 @@ from signxml import XMLSigner, XMLVerifier
 signer = XMLSigner()
 verifier = XMLVerifier()
 
-def sign(tree, key, cert):
-    return signer.sign(tree, key=key, cert=cert)
+def sign(tree, key, cert, **kwargs):
+    return signer.sign(tree, key=key, cert=cert, **kwargs)
 
-def verify(tree):
-    return bool(verifier.verify(tree).signed_xml)
+def verify(tree, **kwargs):
+    return bool(verifier.verify(tree, **kwargs).signed_xml)
