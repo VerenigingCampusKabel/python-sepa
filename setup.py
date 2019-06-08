2c34c448b23ce5a6cacbb0b5b4c48c7b0ce2e44b
Daniel Huisman <daniel@huisman.me>
2019-06-08 19:07:30 +0200

    Remove long_description for release

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open('README.md', 'r') as f:
     readme = f.read()
 
-with open('LICENSE.md') as f:
+with open('LICENSE.md', 'r') as f:
     license = f.read()
 
 setup(
     name='sepa',
     version='0.5.0',
     description='Python library for parsing and building SEPA Direct Debit and SEPA eMandate schemas.',
-    long_description=readme,
+    #long_description=readme,
+    #long_description_content_type='text/markdown',
     license=license,
     author='Vereniging Campus Kabel',
     author_email='info@vck.utwente.nl',
