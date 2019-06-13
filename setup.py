388350b6676d23e6e0378a034064611e52380f8a
Daniel Huisman <daniel@huisman.me>
2019-06-13 15:32:55 +0200

    Release v0.5.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE.md', 'r') as f:
 
 setup(
     name='sepa',
-    version='0.5.0',
+    version='0.5.1',
     description='Python library for parsing and building SEPA Direct Debit and SEPA eMandate schemas.',
     #long_description=readme,
     #long_description_content_type='text/markdown',
