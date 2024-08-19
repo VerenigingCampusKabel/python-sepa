6e154e5fa1d6ad5bbd04388d9a6f4c33400832b1
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2024-08-19 11:31:56 +0300

    Update to Python 12 loader (#7)

--- sepa/messages/pain/__init__.py
+++ sepa/messages/pain/__init__.py
@@ -6,7 +6,7 @@ sepa_messages = {}
 # Loop over modules in this directory
 for loader, name, is_pkg in pkgutil.walk_packages(__path__):
     # Load a module
-    module = loader.find_module(name).load_module(name)
+    module = loader.find_spec(name).loader.load_module(name)
 
     # Export the module
     sepa_messages[name] = module
